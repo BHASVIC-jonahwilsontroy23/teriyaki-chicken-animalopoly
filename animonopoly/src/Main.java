8dc4b7e676b33983e0230cbfbb47ee181dcc7fef
Jonah <jonah.wilsontroy23@students.bhasvic.ac.uk>
2024-03-08 14:00:24 +0000

    aft sch afain 1

--- animonopoly/src/Main.java
+++ animonopoly/src/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Animals Penguin = new Animals(1,100,1,100,"Penguin");
         Animals Polar_Bear = new Animals(2,100,1,100,"Penguin");
         Animals Seal = new Animals(3,125,1,100,"Penguin");
